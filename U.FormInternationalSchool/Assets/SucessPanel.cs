52bc991822915a4c0503f0b093c1c9be86d17e80
ThayFCruz <thy.fer@hotmail.com>
2023-08-14 17:28:36 -0300

    bugfix

--- U.FormInternationalSchool/Assets/SucessPanel.cs
+++ U.FormInternationalSchool/Assets/SucessPanel.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using LubyLib.Core.Singletons;
 using TMPro;
-using UnityEditor.VersionControl;
 using UnityEngine;
 using UnityEngine.UI;
 
